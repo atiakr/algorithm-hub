300cea335ca702eae00f34020a85a244f8288389
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-10-26 20:55:44 +0900

    pgs: 1 / 5m

---
+++ PGS/6_이상한문자만들기.js
@@ -0,0 +1,45 @@
+/**
+문제 설명
+문자열 s는 한 개 이상의 단어로 구성되어 있습니다. 각 단어는 하나 이상의 공백문자로 구분되어 있습니다.
+각 단어의 짝수번째 알파벳은 대문자로, 홀수번째 알파벳은 소문자로 바꾼 문자열을 리턴하는 함수, solution을 완성하세요.
+
+제한 사항
+문자열 전체의 짝/홀수 인덱스가 아니라, 단어(공백을 기준)별로 짝/홀수 인덱스를 판단해야합니다.
+첫 번째 글자는 0번째 인덱스로 보아 짝수번째 알파벳으로 처리해야 합니다.
+
+입출력 예 설명
+"try hello world"는 세 단어 "try", "hello", "world"로 구성되어 있습니다.
+각 단어의 짝수번째 문자를 대문자로, 홀수번째 문자를 소문자로 바꾸면 "TrY", "HeLlO", "WoRlD"입니다.
+따라서 "TrY HeLlO WoRlD" 를 리턴합니다.
+ */
+
+// 내가 푼 방법2 - 한줄로 풀기
+// 0.08ms
+const solution = (str) => {
+    return str
+        .split(" ")
+        .map((el) => el.split(""))
+        .map((el) =>
+            el
+                .map((e, idx) =>
+                    idx % 2 === 0 ? e.toUpperCase() : e.toLowerCase()
+                )
+                .join("")
+        )
+        .join(" ");
+};
+
+// 내가 푼 방법
+// 0.12ms
+const solution = (str) => {
+    const _wordArray = str.split(" ");
+    for (let i = 0; i < _wordArray.length; i++) {
+        _wordArray[i] = _wordArray[i]
+            .split("")
+            .map((el, idx) =>
+                idx % 2 === 0 ? el.toUpperCase() : el.toLowerCase()
+            )
+            .join("");
+    }
+    return _wordArray.join(" ");
+};
