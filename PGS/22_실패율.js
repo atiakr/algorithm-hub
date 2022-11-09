a259cc5bf3322a7fa30b35aca68e05894778e6bb
Atia <67799687+AtiaKR@users.noreply.github.com>
2022-11-09 23:25:09 +0900

    pgs: 1 / 25m

---
+++ PGS/22_실패율.js
@@ -0,0 +1,22 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42889
+
+const solution = (N, stages) => {
+    const stageFailCount = new Array(N).fill(0);
+    const stageClearCount = new Array(N).fill(0);
+    for (let i = 0; i < stages.length; i++) {
+        const _currentStage = stages[i];
+
+        // 스테이지 클리어 카운트를 1씩 더함
+        for (let j = 0; j < _currentStage; j++) {
+            stageClearCount[j]++;
+            stageFailCount[j]++;
+        }
+
+        // 현재 스테이지의 클리어 카운트를 1 뺌
+        stageFailCount[_currentStage - 1] -= 1;
+    }
+
+    // 성공 퍼센테이지를 낮은순서대로 정렬
+    const clearPercentage = stageFailCount.map((el) => el / stageClearCount[j]);
+    return new Array(N).fill().map((_, idx) => idx);
+};
